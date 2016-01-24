39e77261af0080e84c04fa4bf1b65702c472344e
cassidoo <cassidy@clarifai.com>
2016-01-23 22:55:30 -0500

    Update NSFW model

--- protected.py
+++ protected.py
@@ -1,6 +1,6 @@
 from clarifai.client import ClarifaiApi
-clarifai_api = ClarifaiApi(model='nsfw-v0.0')
-result = clarifai_api.tag_image_urls('http://www.clarifai.com/img/metro-north.jpg')
+clarifai_api = ClarifaiApi(model='nsfw-v0.1')
+result = clarifai_api.tag_image_urls('https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg')
 result = result['results'][0]['result']['tag']['probs']
 
 if result[0] > result[1]:
